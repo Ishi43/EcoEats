cc2ebf3accf55c6ccfecf601c38698e2fc76a1e4
Ishika <ishika.meel.17@gmail.com>
2023-10-29 14:46:42 -0400

    Adding Xcode files

---
+++ EcoEats1/EcoEats1/EcoEats1App.swift
@@ -0,0 +1,17 @@
+//
+//  EcoEats1App.swift
+//  EcoEats1
+//
+//  Created by Ishika Meel on 10/29/23.
+//
+
+import SwiftUI
+
+@main
+struct EcoEats1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
